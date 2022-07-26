5a8e39b480ea89f2bafe5453a213f000cd724196
zpengineer <clickprime94@gmail.com>
2022-07-26 17:40:08 +0300

    Correction mentor remarks

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -7,24 +7,27 @@ const checkAuth = async (req, res, next) => {
   const {authorization = ''} = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  try {
-    if (bearer !== 'Bearer') {
-      return res.status(401).json({
-        message: 'Not authorized',
-      });
-    }
-    const {id} = jwt.verify(token, SECRET);
-    const user = await User.findById(id);
+  if (bearer !== 'Bearer') {
+    return res.status(401).json({
+      message: 'Not authorized',
+    });
+  }
 
-    if (!user || !user.token) {
-      return res.status(401).json({
-        message: 'Not authorized',
-      });
+  if (token) {
+    try {
+      const {id} = jwt.verify(token, SECRET);
+      const user = await User.findById(id);
+
+      if (!user || !user.token) {
+        return res.status(401).json({
+          message: 'Not authorized',
+        });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      next(error);
     }
-    req.user = user;
-    next();
-  } catch (error) {
-    next(error);
   }
 };
 
